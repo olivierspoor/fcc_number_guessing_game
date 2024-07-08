91ac5194a8f99156a14148514fce06c332bbfe77
Olivier Spoor <o.spoor@proton.me>
2024-07-08 12:05:23 +0000

    feat: enter username / generate secret number

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo Enter your username:
+read NAME
+
+while [[ -z $NAME ]]
+  do
+    echo Please enter a valid name.
+    read NAME
+  done
+
+PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name='$NAME'")
+
+# new player / insert into db
+if [[ -z $PLAYER_ID ]]
+  then
+  echo Welcome, $NAME! It looks like this is your first time here.
+fi
+
+SECRET_NUMBER=$((1 + $RANDOM % 1000))
