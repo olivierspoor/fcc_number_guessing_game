7927b740d624fcb69c793f8e93965663c0a61e14
Olivier Spoor <o.spoor@proton.me>
2024-07-08 14:33:01 +0000

    feat: insert user in db / guess number

--- number_guess.sh
+++ number_guess.sh
@@ -11,7 +11,7 @@ while [[ -z $NAME ]]
     read NAME
   done
 
-PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name='$NAME'")
+PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username='$NAME'")
 
 # new player / insert into db
 if [[ -z $PLAYER_ID ]]
@@ -19,18 +19,43 @@ if [[ -z $PLAYER_ID ]]
   echo Welcome, $NAME! It looks like this is your first time here.
 
   # insert into players table
+  INSERT_PLAYER_RESULT=$($PSQL "INSERT INTO players(username, games_played) VALUES('$NAME', 0)")
 
-  #else
-  # registered player / welcome back
-
-#   echo Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+  else
+    # registered player / welcome back
+    REGISTERED_PLAYER_RESULT=$($PSQL "SELECT username, games_played, best_game FROM players WHERE username='$NAME'")
+    echo $REGISTERED_PLAYER_RESULT | while IFS='|' read USERNAME GAMES_PLAYED BEST_GAME
+      do
+        echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+      done
 fi
 
 SECRET_NUMBER=$((1 + $RANDOM % 1000))
 
-# while guess != secret_number
-  # too low
-  # too high
+echo $SECRET_NUMBER
+
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
+
+# check integer
+# re = '^[0-9]+$'
+
+while [[ $GUESS != $SECRET_NUMBER ]]
+ do
+
+  if [[ $GUESS < $SECRET_NUMBER ]] 
+  then
+    echo "It's higher than that, guess again:"
+  elif [[ $GUESS > $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  fi
+
+  read GUESS
+ 
+  done
+
+echo Well done.
 
 # correct guess / game finished
 
